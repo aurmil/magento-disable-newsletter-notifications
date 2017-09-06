55f36a35af507aaf6a505e07ccd4b8e8954f6b36
Aurélien Millet <aurmil@users.noreply.github.com>
2017-09-06 10:50:24 +0200

    Do not disable confirm email anymore

--- app/code/community/Aurmil/DisableNewsletterNotifications/Model/Subscriber.php
+++ app/code/community/Aurmil/DisableNewsletterNotifications/Model/Subscriber.php
@@ -8,15 +8,6 @@
 class Aurmil_DisableNewsletterNotifications_Model_Subscriber
 extends Mage_Newsletter_Model_Subscriber
 {
-    public function sendConfirmationRequestEmail()
-    {
-        if (Mage::getStoreConfigFlag('newsletter/subscription/success_email_send')) {
-            return parent::sendConfirmationRequestEmail();
-        }
-
-        return $this;
-    }
-
     public function sendConfirmationSuccessEmail()
     {
         if (Mage::getStoreConfigFlag('newsletter/subscription/success_email_send')) {
